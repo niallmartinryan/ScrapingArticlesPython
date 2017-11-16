b7934dba5a03239053af420d3e175b755d0131f1
Niall Martin Ryan <ryann11@tcd.ie>
2017-11-16 21:47:14 +0000

    added important url stuff in resources and figured out how to crawl the website

--- Resources/resources.py
+++ Resources/resources.py
@@ -2,11 +2,14 @@
 
 # organise these into subgroups separated by comments
 
-#GOOGLE SCHOLAR LINK STRING CONSTANTS
+# GOOGLE SCHOLAR LINK STRING CONSTANTS
 GOOGLE_SCHOLAR_MAIN_PAGE = "http://scholar.google.com"
 
-GOOGLE_SCHOLAR_SEARCH_START = "https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q="
-GOOGLE_SCHOLAR_SEARCH_END = "&btnG="
+GOOGLE_SCHOLAR_SEARCH_START = "https://scholar.google.com/scholar?start=0&q="
+# index of starting article goes here = 0 and goes in increments of 10
+GOOGLE_SCHOLAR_SEARCH_MIDDLE = "&q="
+# Search criteria goes inbetween these strings
+GOOGLE_SCHOLAR_SEARCH_END = "&hl=en&as_sdt=0,5"
 
 SCHOLAR_BIBTEX_LINK_START = "https://scholar.google.com/scholar?q=info:"
 SCHOLAR_BIBTEX_LINK_END = ":scholar.google.com/&output=cite&scirp=1&hl=en"
