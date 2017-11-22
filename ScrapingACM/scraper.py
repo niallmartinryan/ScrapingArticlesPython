9ca9c5b43568b6a2773062e72248ac655925c633
Niall Martin Ryan <ryann11@tcd.ie>
2017-11-22 23:31:17 +0000

    test Scraping on CCS

---
+++ ScrapingACM/scraper.py
@@ -0,0 +1,13 @@
+#  https://dl.acm.org/citation.cfm?doid=965400.111112
+
+
+
+
+
+
+def Main():
+
+
+
+
+Main()
--- ScrapingCCS/scraper.py
+++ ScrapingCCS/scraper.py
@@ -24,45 +24,86 @@ import random
  # x : 7
  # y : 11
 
+# PUT ALL LITERAL DATA STRINGS IN RES FILE...
 
-def getRequest(url, params):
-	req = requests.get(url, params=params)
+def getRequest(url, params, headers):
+	try:
+		req = requests.get(url, params=params)
 
-	soup = BeautifulSoup(req.text, "html.parser")
+		soup = BeautifulSoup(req.text, "html.parser")
 
-	myBibtexPres = soup.findAll("pre",{"class" : "bibtex"})
-	# print(myBibtexPres)
-	print(myBibtexPres[0])
-
+		myBibtexPres = soup.findAll("pre",{"class" : "bibtex"})
+		# print(myBibtexPres)
+		# print(myBibtexPres[0])
+		return myBibtexPres
+	except Exception as e:
+		# print(e)
+		moveOn = true
+		return None
 
 def getRandomMidDelay():
     return random.randint(15,25)
-    
+
 def getRandomUserAgent():
     return res.USER_AGENT_STRING[random.randint(0,len(res.USER_AGENT_STRING))]
 
 
+def getNamesData():
+    with open("finalNames.txt","r") as lines:
+        result = []
+        for line in lines:
+            result.append(line)
+    return result
+
 
 def Main():
-	name_file_test = "data_cid_data.txt"
-	complete_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), name_file_test)
-	file = open(complete_path, "w")
-	
+	path_file_test = "data_cid_data.txt"
+	complete_path_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), path_file_test)
+	file = open(complete_path_data, "w")
 	# https://liinwww.ira.uka.de/csbib/?query=%22Hello%22 
-	# heheheheh
 
+	names = getNamesData()
+
+	userAgent = getRandomUserAgent()
+
+	param_start = 1
+	param_maxnum = 400
+
+	moveOn = false
+	# Only goes up to 1000 in search 
 	url = "https://liinwww.ira.uka.de/csbib/"
 	# will need to add to this
-	params = {'query' : 'angela',
+	params = {
+			'query' : names[0],
 			'results' : 'bibtex',
-			'sort' : 'year'
+			'sort' : 'year',
+			'start' : str(param_start) ,
+			'maxnum' : str(param_maxnum),
 			}
 	headers = {
-
+			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.8',
+            'upgrade-insecure-requests': '1',
+            'user-agent': userAgent
 	}
-	for
-	getRequest(url, params)
+	# for name in names:
+	while moveOn == false:
+		
+		params = {
+			# 'query' : name,
+			'query' : names[0],
+			'results' : 'bibtex',
+			'sort' : 'year',
+			'start' : str(param_start) ,
+			'maxnum' : str(param_maxnum),
+			}	
+	# at the end of this while.. I would need to reinitialise moveOn to true.. or maybe even before this while loop
+		bibtexData = getRequest(url, params, headers)
+		param_start += param_maxnum 
+
 	
+
 
 	# file.write(str(req.text))
 
