9d3bb31bb5f8032b36a83567feb91d075205a582
Niall Martin Ryan <ryann11@tcd.ie>
2017-11-22 09:57:32 +0000

    Added randomRateDelay

--- scraping.py
+++ scraping.py
@@ -26,6 +26,9 @@ def getRandomShortDelay():
 def getRandomStandardDelay():
     return random.randint(20, 40)
 
+def getRandomRateDelay():
+    return random.randint(30,50)
+
 def getRandomUserAgent():
     return res.USER_AGENT_STRING[random.randint(0,len(res.USER_AGENT_STRING))]
 
@@ -88,22 +91,6 @@ def getBibtexURL(url):
     data_cid_list = []
     try:
         userAgent = getRandomUserAgent()
-        # url = 'https://scholar.google.pl/citations?view_op=search_authors&mauthors=label:security'
-
-        # request_headers = {
-        #     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        #     'accept-encoding': 'gzip, deflate, br',
-        #     'accept-language': 'en-US,en;q=0.8',
-        #     'upgrade-insecure-requests': '1',
-        #     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-        # } 
-        # s.get(url, headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        #     'accept-encoding': 'gzip, deflate, br',
-        #     'accept-language': 'en-US,en;q=0.8',
-        #     'upgrade-insecure-requests': '1',
-        #     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-        # })
-
         headers = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'en-US,en;q=0.8',
