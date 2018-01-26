cabf5bf5a9f956c5f41d6047e42c86650d64edee
niallmartinryan <ryann11@tcd.ie>
2018-01-26 17:44:17 +0000

    Adding scraping of artificial journal names

---
+++ ScrapingArtificial/scimagojr/scraper.py
@@ -0,0 +1,48 @@
+# Scraping Artificial Journal names from scimagojr
+import sys
+import os.path
+import time
+import res
+from bs4 import BeautifulSoup
+import urllib
+import urllib.parse
+import urllib.request
+import requests
+import random
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def Main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+Main()
